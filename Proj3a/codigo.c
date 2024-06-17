3348cdf44a46e4178468791023f246ca0fc214c6
almdoug <douglassurf2014@gmail.com>
2024-06-17 11:35:08 -0300

    Proj3a

---
+++ Proj3a/codigo.c
@@ -0,0 +1,44 @@
+// Douglas Benevides Almeida
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void permutacao(int vetor[], int n, int i) {
+    if (i == n) {
+        return;
+    }
+    for (int j = i; j < n; j++) {
+        int temp = vetor[i];
+        vetor[i] = vetor[j];
+        vetor[j] = temp;
+        permutacao(vetor, n, i + 1);
+        temp = vetor[i];
+        vetor[i] = vetor[j];
+        vetor[j] = temp; 
+    }
+}
+
+int main() {
+    int max_n = 2;
+    clock_t inicio, fim;
+    printf("Tempo de permutacao para cada n:\n");
+    while (1) {
+        int vetor[max_n];
+        for (int i = 0; i < max_n; i++) {
+            vetor[i] = i + 1;
+        }
+
+        inicio = clock();
+        permutacao(vetor, max_n, 0);
+        fim = clock();
+
+        double tempo = ((double) (fim - inicio)) / CLOCKS_PER_SEC;
+        printf("n = %d: --- %.6f segundos\n", max_n, tempo);
+
+        if (tempo > 60.0) {
+            break;
+        }max_n++;
+    }
+    printf("--> n maximo em menos de 1 minuto: %d\n", max_n - 1);
+    return 0;
+}
---
+++ Proj3a/codigo.java
@@ -0,0 +1,44 @@
+// Douglas Benevides Almeida
+import java.util.Arrays;
+
+public class Main {
+    static void permutacao(int[] vetor, int n, int i) {
+        if (i == n) {
+            return;
+        }
+        for (int j = i; j < n; j++) {
+            int temp = vetor[i];
+            vetor[i] = vetor[j];
+            vetor[j] = temp;
+            permutacao(vetor, n, i + 1);
+            temp = vetor[i];
+            vetor[i] = vetor[j];
+            vetor[j] = temp; 
+        }
+    }
+
+    public static void main(String[] args) {
+        int max_n = 2;
+        long inicio, fim;
+        System.out.println("Tempo de permutacao para cada n:");
+
+        while (true) {
+            int[] vetor = new int[max_n];
+            for (int i = 0; i < max_n; i++) {
+                vetor[i] = i + 1;
+            }
+
+            inicio = System.currentTimeMillis();
+            permutacao(vetor, max_n, 0);
+            fim = System.currentTimeMillis();
+
+            double tempo = (fim - inicio) / 1000.0;
+            System.out.printf("n = %d: --- %.6f segundos\n", max_n, tempo);
+            if (tempo > 60.0) {
+                break;
+            }
+            max_n++;
+        }
+        System.out.printf("--> n maximo em menos de 1 minuto: %d\n", max_n - 1);
+    }
+}
